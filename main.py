487e72575ffc23bcc0149e3b5d16c8993641b469
omaimasharhan <omaimasharhan@gmail.com>
2023-09-01 08:07:47 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#PART 1
+def list_divide(numbers, divide = 2):
+    elements = 0
+    for num in numbers:
+        if num % divide == 0:
+            elements +=1
+    return elements
+class ListDivideException(Exception):
+    pass
+def test_list_divide():
+    try:
+        assert list_divide([1, 2, 3, 4, 5]) == 2
+        assert list_divide([2, 4, 6, 8, 10]) == 5
+        assert list_divide([30, 54, 63, 98, 100], divide=10) == 2
+        assert list_divide([]) == 0
+        assert list_divide([1, 2, 3, 4, 5], 1) == 5
+    except AssertionError:
+        raise ListDivideException ("Test Failed")
+
+test_list_divide()
