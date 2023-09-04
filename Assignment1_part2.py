94c41a0f305a3eaf9452172c33c269192c7a7e26
Omaima Sharhan <126303348+omaimasharhan@users.noreply.github.com>
2023-09-04 03:51:59 +0300

    Update Assignment1_part2.py

--- Assignment1_part2.py
+++ Assignment1_part2.py
@@ -1,10 +1,1 @@
-class Book:
-    def __init__(self, author, title):
-        self.author = author
-        self.title = title
 
-    def display(self):
-        print(f"{self.title}, written by {self.author}")
-
-book1 = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
-book2 = Book("Walter Scott", "Ivanhoe: A Romance")
